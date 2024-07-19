e3ad9a787cbb6c8c8ba17c057d54f8d70ca0ca0e
Clark Wimberly <clark@clarklab.net>
2024-07-19 16:44:29 -0500

    init

--- src/components/Machine.vue
+++ src/components/Machine.vue
@@ -1,72 +1,106 @@
 <template>
-  <div class="wrapper">
-    <div class="flex flex-col md:flex-row justify-center gap-3 md:gap-8 mb-8 xl:gap-16 md:mb-16">
+    <div class="flex flex-col w-full px-4 md:flex-row justify-center gap-3 md:gap-8 mb-8 xl:gap-16 md:mb-16">
       <div>
-        <div class="font-michroma text-left md:text-center mb-4 text-base md:text-sm xl:text-base">
-          drums
+        <div class="font-michroma text-left md:text-center mb-2 mt-6 text-sm md:text-sm xl:text-base uppercase tracking-widest text-gray-400">
+          Night
         </div>
 
         <div class="flex flex-row md:grid md:grid-cols-2 gap-3 md:gap-2 xl:gap-4">
           <button
-            v-for="player in getPlayersByType('drums')"
+            v-for="player in getPlayersByType('night')"
             :key="player.sample.id"
             :disabled="!player.sample.ready"
             class="focus:outline-none button button-music button-blue"
             :class="{ active: player.howl.volume() == 1 }"
             @click="toggle(player.sample.id)"
-          />
+          >
+            <span class="material-symbols-outlined">{{ player.sample.icon }}</span>
+            {{ player.sample.name }}
+          </button>
         </div>
       </div>
       <div>
-        <div class="font-michroma text-left md:text-center mb-4 text-base md:text-sm xl:text-base">
-          bass
+        <div class="font-michroma text-left md:text-center mb-2 mt-6 text-sm md:text-sm xl:text-base uppercase tracking-widest text-gray-400">
+          Howls
         </div>
 
         <div class="flex flex-row md:grid md:grid-cols-2 gap-3 md:gap-2 xl:gap-4">
           <button
-            v-for="player in getPlayersByType('bass')"
+            v-for="player in getPlayersByType('howl')"
             :key="player.sample.id"
             :disabled="!player.sample.ready"
             class="focus:outline-none button button-music button-red"
             :class="{ active: player.howl.volume() == 1 }"
             @click="toggle(player.sample.id)"
-          />
+          >
+            <span class="material-symbols-outlined">{{ player.sample.icon }}</span>
+            {{ player.sample.name }}
+          </button>
         </div>
       </div>
       <div>
-        <div class="font-michroma text-left md:text-center mb-4 text-base md:text-sm xl:text-base">
-          vocals
+        <div class="font-michroma text-left md:text-center mb-2 mt-6 text-sm md:text-sm xl:text-base uppercase tracking-widest text-gray-400">
+          Werewolf Attack
         </div>
 
         <div class="flex flex-row md:grid md:grid-cols-2 gap-3 md:gap-2 xl:gap-4">
           <button
-            v-for="player in getPlayersByType('vocals')"
+            v-for="player in getPlayersByType('hunt')"
             :key="player.sample.id"
             :disabled="!player.sample.ready"
             class="focus:outline-none button button-music button-green"
             :class="{ active: player.howl.volume() == 1 }"
             @click="toggle(player.sample.id)"
-          />
+          >
+            <span class="material-symbols-outlined">{{ player.sample.icon }}</span>
+            {{ player.sample.name }}
+          </button>
         </div>
+
       </div>
       <div>
-        <div class="font-michroma text-left md:text-center mb-4 text-base md:text-sm xl:text-base">
-          other
+        <div class="font-michroma text-left md:text-center mb-2 mt-6 text-sm md:text-sm xl:text-base uppercase tracking-widest text-gray-400">
+          Morning
         </div>
 
         <div class="flex flex-row md:grid md:grid-cols-2 gap-3 md:gap-2 xl:gap-4">
           <button
-            v-for="player in getPlayersByType('other')"
+            v-for="player in getPlayersByType('morning')"
             :key="player.sample.id"
             :disabled="!player.sample.ready"
             class="focus:outline-none button button-music button-yellow"
             :class="{ active: player.howl.volume() == 1 }"
             @click="toggle(player.sample.id)"
-          />
+          >
+            <span class="material-symbols-outlined">{{ player.sample.icon }}</span>
+            {{ player.sample.name }}
+          </button>
         </div>
       </div>
+
+      <div>
+        <div class="font-michroma text-left md:text-center mb-2 mt-6 text-sm md:text-sm xl:text-base uppercase tracking-widest text-gray-400">
+          Misc
+        </div>
+
+        <div class="flex flex-row md:grid md:grid-cols-2 gap-3 md:gap-2 xl:gap-4">
+          <button
+            v-for="player in getPlayersByType('misc')"
+            :key="player.sample.id"
+            :disabled="!player.sample.ready"
+            class="focus:outline-none button button-music button-purple"
+            :class="{ active: player.howl.volume() == 1 }"
+            @click="toggle(player.sample.id)"
+          >
+            <span class="material-symbols-outlined">{{ player.sample.icon }}</span>
+            {{ player.sample.name }}
+          </button>
+        </div>
+      </div>
+
     </div>
-  </div>
+
+
 </template>
 
 <script>
@@ -83,39 +117,37 @@ export default {
 
   async mounted() {
     // Audio
-    await this.loadSample( 4, 'bass', 'bass_chance', '/audio/bass_chance.mp3')
-    await this.loadSample( 5, 'drums', 'drums_chance', '/audio/drums_chance.mp3')
-    await this.loadSample( 6, 'vocals', 'vocals_chance', '/audio/vocals_chance.mp3')
-    await this.loadSample( 7, 'other', 'other_chance', '/audio/other_chance.mp3')
+    await this.loadSample( 4, 'night', 'Rain', 'rainy', '/audio/night_1.mp3')
+    await this.loadSample( 5, 'howl', 'Howl', 'looks_one', '/audio/howl_1.mp3')
+    await this.loadSample( 6, 'hunt', 'Scent', 'heat', '/audio/hunt_1.mp3')
+    await this.loadSample( 7, 'morning', 'Song', 'sunny', '/audio/morning_1.mp3')
 
-    await this.loadSample( 8, 'bass', 'bass_lapaix', '/audio/bass_lapaix.mp3')
-    await this.loadSample( 9, 'drums', 'drums_lapaix', '/audio/drums_lapaix.mp3')
-    await this.loadSample(10, 'vocals', 'vocals_lapaix', '/audio/vocals_lapaix.mp3')
-    await this.loadSample(11, 'other', 'other_lapaix', '/audio/other_lapaix.mp3')
+    await this.loadSample( 8, 'night', 'Didge', 'graphic_eq', '/audio/night_2.mp3')
+    await this.loadSample( 9, 'howl', 'Howl', 'looks_two', '/audio/howl_2.mp3')
+    await this.loadSample(10, 'hunt', 'Growl', 'hearing', '/audio/hunt_2.mp3')
+    await this.loadSample(11, 'morning', 'Birds', 'raven', '/audio/morning_2.mp3')
 
-    await this.loadSample(12, 'bass', 'bass_nikki', '/audio/bass_nikki.mp3')
-    await this.loadSample(13, 'drums', 'drums_nikki', '/audio/drums_nikki.mp3')
-    await this.loadSample(14, 'vocals', 'vocals_nikki', '/audio/vocals_nikki.mp3')
-    await this.loadSample(15, 'other', 'other_nikki', '/audio/other_nikki.mp3')
+    await this.loadSample(12, 'night', 'Crickets', 'pest_control', '/audio/night_3.mp3')
+    await this.loadSample(13, 'howl', 'Howl', 'looks_3', '/audio/howl_3.mp3')
+    await this.loadSample(14, 'hunt', 'Attack', 'emergency_home', '/audio/hunt_3.mp3')
+    await this.loadSample(15, 'morning', 'Piano', 'piano', '/audio/morning_3.mp3')
 
-    await this.loadSample(16, 'bass', 'bass_boum_bap', '/audio/bass_boum_bap.mp3')
-    await this.loadSample(17, 'drums', 'drums_boum_bap', '/audio/drums_boum_bap.mp3')
-    await this.loadSample(18, 'vocals', 'vocals_boum_bap', '/audio/vocals_boum_bap.mp3')
-    await this.loadSample(19, 'other', 'other_boum_bap', '/audio/other_boum_bap.mp3')
+    await this.loadSample( 1, 'night', 'Horror', 'comedy_mask', '/audio/night_4.mp3')
+    await this.loadSample( 2, 'howl', 'Howl', 'looks_4', '/audio/howl_4.mp3')
+    await this.loadSample( 3, 'morning', 'Rooster', 'campaign', '/audio/morning_4.mp3')
+    await this.loadSample(16, 'hunt', 'Siren', 'notifications_active', '/audio/hunt_4.mp3')
 
-    await this.loadSample( 1, 'bass', 'bass_2019', '/audio/bass_2019.mp3')
-    await this.loadSample( 2, 'drums', 'drums_2019', '/audio/drums_2019.mp3')
-    await this.loadSample( 3, 'other', 'other_2019', '/audio/other_2019.mp3')
+    await this.loadSample( 17, 'misc', 'Boos', 'thumb_down', '/audio/misc_1.mp3')
+    await this.loadSample( 18, 'misc', 'Mob', 'groups_2', '/audio/misc_2.mp3')
+    await this.loadSample( 19, 'misc', 'Shotgun', 'target', '/audio/misc_3.mp3')
+    await this.loadSample( 20, 'misc', 'Laughs', 'sentiment_very_satisfied', '/audio/misc_4.mp3')
 
-    // await this.loadSample(20, 'drums', 'drums_longfeu', '/audio/drums_longfeu.mp3')
-    // await this.loadSample(21, 'vocals', 'vocals_longfeu', '/audio/vocals_longfeu.mp3')
-    // await this.loadSample(22, 'other', 'other_longfeu', '/audio/other_longfeu.mp3')
 
     setTimeout(this.start, 100)
   },
 
   methods: {
-    async loadSample(id, type, name, url){
+    async loadSample(id, type, name, icon, url){
       let enabledIds = []
 
       let hash = window.location.hash.substr(1)
@@ -138,6 +170,7 @@ export default {
         sample: {
           id: id,
           name: name,
+          icon: icon,
           type: type,
           url: url,
           ready: false,
